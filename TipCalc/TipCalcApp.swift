63f82c1876ce2b98bb8f24ffc99e39f072ae6908
Joseph Ollis <35369747+DasTib@users.noreply.github.com>
2023-07-07 19:19:56 -0500

    Initial Commit

---
+++ TipCalc/TipCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalcApp.swift
+//  TipCalc
+//
+//  Created by Joseph Ollis on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TipCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
